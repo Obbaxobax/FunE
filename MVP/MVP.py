4d8efcd09940ebee8792cdced4bcd87a54253687
obbax_obax <70305616+Obbaxobax@users.noreply.github.com>
2023-06-24 20:45:36 -0600

    Add files via upload
    Changed the way games are saved to make for easier implementation. Images coming soon...

--- MVP/MVP.py
+++ MVP/MVP.py
@@ -3,21 +3,32 @@ import json
 import os
 from time import sleep
 from threading import Thread
+from google_images_search import GoogleImagesSearch
 import pythoncom
+
+pathz = 'Game Cover Arts'
+DK = 'AIzaSyBkwVyCi125GHnCXpqE3p1oTOH59I5zAmM'
+CX = '25777af3020ad413b'
+
+active = True
 
 def getProcesses():
     pythoncom.CoInitialize()
     global big_process
+    global big_path
+    global active
 
     w = wmi.WMI()
     
     process_watcher = w.Win32_Process.watch_for("creation")
-    while True:
+    while active:
         sleep(1)
         new_process = process_watcher()
         big_process = new_process.Caption.lower()
+        big_path = new_process.CommandLine
 
 big_process = ""
+big_path = ""
 
 try:
     with open('games.txt') as t:
@@ -28,7 +39,7 @@ try:
         else:
             savedGames = {}
     
-    games = []
+    games = {}
     names = open('detectable.json')
     data = json.load(names)
 
@@ -47,8 +58,8 @@ try:
             if g['os'] != 'win32':
                 continue
 
-            splited = g['name'].split("/")
-            games.append(splited[len(splited)-1].lower())
+            splited = g['name'].split("/") 
+            games[splited[len(splited)-1].lower()] = i['name']
 
     #Funny library
     flag = False
@@ -71,15 +82,17 @@ try:
 
             if not flag:
                 #temp = new_process.CommandLine.replace("\"", '')
-                temp = (new_process.CommandLine.split('"')[1])
+                temp = (big_path.split('"')[1])
                 print(temp)
-                savedGames[funny] = temp
+                savedGames[games[funny]] = [funny, temp]
                 with open("games.txt", "w") as t:
                     json.dump(savedGames, t)
             else:
                 flag = False
     
 finally:
+    active = False
+    
     with open("games.txt", "w") as t:
         json.dump(savedGames, t)
     print("written to file")
