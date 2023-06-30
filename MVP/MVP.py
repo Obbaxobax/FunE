eab2306e1fa123289fd0f1eaceabf89fe4336537
obbax_obax <70305616+Obbaxobax@users.noreply.github.com>
2023-06-29 21:53:06 -0600

    Added Image Grabbing from IGDB
    Also made txt's json.

--- MVP/MVP.py
+++ MVP/MVP.py
@@ -3,13 +3,29 @@ import json
 import os
 from time import sleep
 from threading import Thread
-from google_images_search import GoogleImagesSearch
 import pythoncom
+import requests
 
 #-----------------API Stuff-----------------------
-pathz = 'Game Cover Arts'
-DK = 'AIzaSyBkwVyCi125GHnCXpqE3p1oTOH59I5zAmM'
-CX = '25777af3020ad413b'
+respo = requests.post("https://id.twitch.tv/oauth2/token?client_id=71mpsyy8zm8q94br1qtu3qco7ms9ez&client_secret=2v1cwaj8e39hxgi61fubwbnruji45i&grant_type=client_credentials")
+token = respo.json()['access_token']
+
+def getNewToken(timeUntilExpired):
+    global token
+
+    #Funny Sleep Hack
+    for i in range(0, 4):
+        sleep(timeUntilExpired / 5)
+    
+    respo = requests.post("https://id.twitch.tv/oauth2/token?client_id=71mpsyy8zm8q94br1qtu3qco7ms9ez&client_secret=2v1cwaj8e39hxgi61fubwbnruji45i&grant_type=client_credentials")
+    token = respo.json()['access_token']
+
+    t = Thread(target=getNewToken, args=[respo.json()['expires_in']])
+    t.start()
+
+t = Thread(target=getNewToken, args=[respo.json()['expires_in']])
+t.start()
+
 
 #-------------Process Detection-------------------
 active = True
@@ -35,15 +51,14 @@ big_path = ""
 
 #-----------------Main Script----------------------
 try:
-    with open('games.txt') as t:
+    with open('games.json') as t:
         text = t.read()
         if text and text != "{}":
             savedGames = json.loads(text)
         else:
             savedGames = {}
     
-    games = {}
-    names = open('detectablesV2.txt')
+    names = open('detectablesV2.json')
     games = json.load(names)
 
     def getGameFromList(name):
@@ -52,7 +67,7 @@ try:
                 return i
         return False
 
-    #--------------Main Loop----------------------
+#-----------------Main Loop----------------------
     savedFlag = False
 
     x = Thread(target=getProcesses)
@@ -72,11 +87,29 @@ try:
 
             if not savedFlag:
                 temp = (big_path.split('"')[1])
-
                 savedGames[games[checkedGame]] = [checkedGame, temp]
 
+                #Get Api Images
+                parms = {
+                    "Client-ID":"71mpsyy8zm8q94br1qtu3qco7ms9ez",
+                    "Authorization": 'Bearer ' + token,
+                }
+
+                games = requests.post("https://api.igdb.com/v4/games",
+                                                          headers=parms,
+                                                          data='fields name, cover; where version_parent = null & category = 0; search "' + 'Titanfall 2' + '";')
+
+
+                cover = requests.post("https://api.igdb.com/v4/covers",
+                                                          headers=parms,
+                                                          data='fields image_id; where id = ' + str(games.json()[0]['cover']) + ';') #str(games.json()[0]['cover']) < What the fuck
+
+                img_data = requests.get('https://images.igdb.com/igdb/image/upload/t_cover_big/' + cover.json()[0]['image_id'] + '.jpg').content
+                with open('Cover Images/' + games.json()[0]['name'] + '.jpg', 'wb') as temp:
+                        temp.write(img_data)                
+
                 #Save
-                with open("games.txt", "w") as t:
+                with open("games.json", "w") as t:
                     json.dump(savedGames, t)
             else:
                 savedFlag = False
@@ -84,7 +117,7 @@ try:
 finally:
     active = False
     
-    with open("games.txt", "w") as t:
+    with open("games.json", "w") as t:
         json.dump(savedGames, t)
     print("written to file")
     
